02967824f2631b9844f45f8d66585e7e6d2e2145
lishile <lishile@gyyx.cn>
2026-05-14 15:49:17 +0800

    单文件备份,不用多文件

--- AcerLaptopRclone/backup.sh
+++ AcerLaptopRclone/backup.sh
@@ -1,20 +1,27 @@
 
 rclone_config_file="C:\Users\lishi\AppData\Roaming\rclone\rclone.conf"
 
-target_file_name=rclone_$(date +'%Y%m%d_%H%M%S').conf
-
 git_repo_base_path="C:\Users\lishi\work\code\keepassbackup"
 
-cd "C:\Users\\lishi\work\code\keepassbackup\AcerLaptopRclone"
+cd ${git_repo_base_path}
+ 
 
 git reset --hard
 
 git pull
 
-cp ${rclone_config_file} ${target_file_name}
+cd AcerLaptopRclone
+
+rm -rf rclone.conf
+
+cp ${rclone_config_file} rclone.conf
 
 git add .
 
-git commit -m "backup acer rclone config file"
+if git diff --cached --quiet; then
+	echo "No changes."
+else
+	git commit -m "backup acer rclone config file $(date -u '+%Y-%m-%d %H:%M:%S UTC')"
+	git push
+fi
 
-git push
